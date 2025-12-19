962a54aaa86b2a6188133fb4624dcb413d417bf5
sam-w-yellin <sam.w.yellin@gmail.com>
2025-12-19 06:20:11 +0000

    Deploying to gh-pages from @ sam-w-yellin/crunch@b69e1a04aa829e5bb4a1f4949ecb828b8c3f82e2 🚀

---
+++ navtreedata.js
@@ -0,0 +1,53 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Crunch", "index.html", [
+    [ "Key Features", "index.html#autotoc_md1", null ],
+    [ "Validation Flow", "index.html#autotoc_md2", null ],
+    [ "Dependencies", "index.html#autotoc_md3", null ],
+    [ "Message Definition & Fields", "index.html#autotoc_md4", null ],
+    [ "Example Usage", "index.html#autotoc_md5", null ],
+    [ "Serialization Layouts", "index.html#autotoc_md6", [
+      [ "Header", "index.html#autotoc_md7", null ],
+      [ "Static Layout", "index.html#autotoc_md8", [
+        [ "Example: Alignment = 4", "index.html#autotoc_md9", null ]
+      ] ],
+      [ "Tag-Length-Value (TLV) Layout", "index.html#autotoc_md10", [
+        [ "Wire Types", "index.html#autotoc_md11", null ],
+        [ "Example: TLV Layout", "index.html#autotoc_md12", null ]
+      ] ]
+    ] ],
+    [ "Roadmap", "index.html#autotoc_md13", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex0.js
@@ -0,0 +1,18 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"index.html#autotoc_md1":[0],
+"index.html#autotoc_md10":[5,2],
+"index.html#autotoc_md11":[5,2,0],
+"index.html#autotoc_md12":[5,2,1],
+"index.html#autotoc_md13":[6],
+"index.html#autotoc_md2":[1],
+"index.html#autotoc_md3":[2],
+"index.html#autotoc_md4":[3],
+"index.html#autotoc_md5":[4],
+"index.html#autotoc_md6":[5],
+"index.html#autotoc_md7":[5,0],
+"index.html#autotoc_md8":[5,1],
+"index.html#autotoc_md9":[5,1,0],
+"pages.html":[]
+};
